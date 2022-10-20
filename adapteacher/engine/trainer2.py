21a938046bc413f80bcf2ed51314724a2f08043e
sina malakouti <siinamalakouti@gmail.com>
2022-10-20 09:40:52 -0400

    Update trainer2.py

--- adapteacher/engine/trainer2.py
+++ adapteacher/engine/trainer2.py
@@ -294,10 +294,10 @@ class ATeacherTrainer(DefaultTrainer):
         # create an teacher model
         s1_head = Custom_head(proposal_generator=None, roi_heads=None, cfg=cfg,
                               backbone_output_shape=model.backbone.output_shape(),
-                              vis_period=0).to('cuda')
+                              vis_period=0)#.to('cuda')
         s2_head = Custom_head(proposal_generator=None, roi_heads=None, cfg=cfg,
                               backbone_output_shape=model.backbone.output_shape(),
-                              vis_period=0).to('cuda')
+                              vis_period=0)#.to('cuda')
 
 
         # For training, wrap with DDP. But don't need this for inference.
@@ -833,21 +833,21 @@ class ATeacherTrainer(DefaultTrainer):
                 )
             )
 
-        def test_and_save_results_student():
-            self._last_eval_results_student = self.test(self.cfg, self.model)
-            _last_eval_results_student = {
-                k + "_student": self._last_eval_results_student[k]
-                for k in self._last_eval_results_student.keys()
-            }
-            return _last_eval_results_student
+        # def test_and_save_results_student():
+        #     self._last_eval_results_student = self.test(self.cfg, self.model)
+        #     _last_eval_results_student = {
+        #         k + "_student": self._last_eval_results_student[k]
+        #         for k in self._last_eval_results_student.keys()
+        #     }
+        #     return _last_eval_results_student
 
         def test_and_save_results_teacher():
             self._last_eval_results_teacher = self.test(
-                self.cfg, self.model_student)
+                self.cfg, self.model)
             return self._last_eval_results_teacher
 
-        ret.append(hooks.EvalHook(cfg.TEST.EVAL_PERIOD,
-                   test_and_save_results_student))
+        # ret.append(hooks.EvalHook(cfg.TEST.EVAL_PERIOD,
+        #            test_and_save_results_student))
         ret.append(hooks.EvalHook(cfg.TEST.EVAL_PERIOD,
                    test_and_save_results_teacher))
 
